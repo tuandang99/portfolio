3eb817bd11114783425bb09012dd46526c13c357
tuan <tuan.dang99@hcmut.edu.vn>
2022-03-25 23:49:25 +0700

    header and home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,42 +6,42 @@ function Header() {
     <div className="wrap-header">
       <div className="navbar">
         <a href="/#">
-          <h3 className="logo">Tuan</h3>
+          <h3 className="logo">
+            Tuan<span style={{color:'red'}}>.</span>
+          </h3>
         </a>
         <div className="menu">
           <ul className="menu-links">
-            <li className="menu-link">
-              <a className="menu-link" href="/#">Home</a>
+            <li>
+              <a className="menu-link active" href="/#">
+                Home
+              </a>
             </li>
             <li>
-              <a className="menu-link" href="/#">About</a>
+              <a className="menu-link" href="/#">
+                About
+              </a>
             </li>
             <li>
-              <a className="menu-link" href="/#">Portfolia</a>
+              <a className="menu-link" href="/#">
+                Portfolio
+              </a>
             </li>
             <li>
-              <a className="menu-link" href="/#">Project</a>
+              <a className="menu-link" href="/#">
+                Project
+              </a>
             </li>
             <li>
-              <a className="menu-link" href="/#">Contact</a>
+              <a className="menu-link" href="/#">
+                Contact
+              </a>
             </li>
           </ul>
         </div>
-        <div className="social">
-          <a href="/#">
-            <i class="fa-brands fa-facebook-f"></i>
-          </a>
-          <a href="/#">
-            <i class="fa-brands fa-github"></i>
-          </a>
-          <a href="/#">
-            <i class="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="/#">
-            <i class="fa-brands fa-instagram"></i>
-          </a>
-          
-        </div>
+        <h3 className="head-footer">
+            Make by heart
+        </h3>
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,33 @@
-import './home.scss'
-import React from 'react'
+import "./home.scss";
+import React from "react";
 
 function Home() {
   return (
-    <div className='home'>Home</div>
-  )
+    <div className="home">
+      <div className="avatar mb-2r"></div>
+      <h1 className="name mb-2r">Dang Anh Tuan</h1>
+      <p className="job mb-2r">I'm a Front-End developer</p>
+      <div className="social mb-2r">
+        <a className="social-logo" href="/#">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a className="social-logo" href="/#">
+          <i class="fa-brands fa-facebook-f"></i>
+        </a>
+        <a className="social-logo" href="/#">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a className="social-logo" href="/#">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+      <button className="hire">Hire me</button>
+      <div className="scroll">
+        <p>Scroll down</p>
+        <i class="fa-solid fa-computer-mouse"></i>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
